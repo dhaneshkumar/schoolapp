eb5b4167bdc7105a4b6cb9464d5c5f511c953c39
prit91 <pncooldude91@gmail.com>
2014-02-09 23:55:17 +0530

    Leave app incorporated

---
+++ schoolapp-master/schoolapp/src/library/init.java
@@ -0,0 +1,10 @@
+package library;
+
+import android.app.Activity;
+
+
+public class init extends Activity{
+	public DatabaseHandler db= new DatabaseHandler(this);
+	public UserFunctions uf = new UserFunctions();
+
+}
